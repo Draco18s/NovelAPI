bf557b6ceb9e961eb018a558794ad6dcbdc8afc7
Jason Walker <jlwalker@r12mail.sps.org>
2024-04-30 14:05:57 -0500

    Add models for User Objects

---
+++ models/NaiObjectResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace net.novelai.api
+{
+    public class NaiObjectResponse : NaiApiError
+    {
+        public IEnumerable<NaiUserData> Objects { get; set; }
+    }
+}
